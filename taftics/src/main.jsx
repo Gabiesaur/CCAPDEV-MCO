feb52c536bd124991c1d0cba6e5fdb4a7cb535bf
natzbtw <leelancze1111@gmail.com>
2026-02-12 23:54:42 +0800

    revert main

--- taftics/src/main.jsx
+++ taftics/src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Establishment from "./pages/Establishment.jsx";
 import "./custom.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Establishment />
+    <App />
   </StrictMode>,
 );
