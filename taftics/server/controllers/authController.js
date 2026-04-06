a246a74740343ec67c9738799e64b9672f51a866
natzbtw <leelancze1111@gmail.com>
2026-04-06 18:46:49 +0800

    apply fix attempt #2

--- taftics/server/controllers/authController.js
+++ taftics/server/controllers/authController.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const Establishment = require("../models/Establishment");
 
 exports.login = async (req, res) => {
   const loginIdentifier = req.body.username || req.body.email;
